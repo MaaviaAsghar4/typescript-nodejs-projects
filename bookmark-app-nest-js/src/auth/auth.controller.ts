2b6e9dd39d6f5043b86c1f45d59a4d45568c9a01
MaaviaAsghar4 <maavia.asghar.96@gmail.com>
2023-01-12 12:57:50 +0500

    auth guard and decorator added

--- bookmark-app-nest-js/src/auth/auth.controller.ts
+++ bookmark-app-nest-js/src/auth/auth.controller.ts
@@ -1,13 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDTO, SignUpDTO } from './dto';
-// import { PrismaClient } from '@prisma/client';
-// const prisma = new PrismaClient();
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   login(@Body() dto: SignInDTO) {
     return this.authService.login(dto);
